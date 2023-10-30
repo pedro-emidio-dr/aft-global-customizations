82426311cac7f8149d0cf0860a3394ca39f4f568
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-10-30 16:22:54 -0300

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,16 @@
-resource "aws_accessanalyzer_analyzer" "main_analyzer" {
-  analyzer_name = "analyzer-${data.aws_caller_identity.current.account_id}"
+locals {
+  analyzer_name = "main_analyzer"
+}
+
+resource "aws_accessanalyzer_analyzer_org" "main_analyzer" {
+  # depends_on = [aws_organizations_organization.example]
+
+  analyzer_name = local.analyzer_name
+  type          = "controller"
+}
+resource "aws_accessanalyzer_analyzer_account" "main_analyzer" {
+  # depends_on = [aws_organizations_organization.example]
+
+  analyzer_name = local.analyzer_name
+  type          = data.aws_caller_identity.current.account_id
 }
