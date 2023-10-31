8869cbca26aba53894c586b60be38e75acd1bb31
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-10-31 12:06:21 -0300

    teste

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,14 +3,10 @@ locals {
 }
 
 resource "aws_accessanalyzer_analyzer_org" "main_analyzer" {
-  # depends_on = [aws_organizations_organization.example]
-
   analyzer_name = local.analyzer_name
   type          = "controller"
 }
 resource "aws_accessanalyzer_analyzer_account" "main_analyzer" {
-  # depends_on = [aws_organizations_organization.example]
-
   analyzer_name = local.analyzer_name
   type          = data.aws_caller_identity.current.account_id
 }
