1b672536bd062b4bace842d93c0916939f6cd9ac
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-11-03 14:42:24 -0300

    deploy bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,20 @@
-locals {
-  analyzer_name = "main_analyzer"
-}
+# locals {
+#   analyzer_name = "main_analyzer"
+# }
 
-resource "aws_accessanalyzer_analyzer_org" "main_analyzer" {
-  analyzer_name = local.analyzer_name
-  type          = "controller"
-}
-resource "aws_accessanalyzer_analyzer_account" "main_analyzer" {
-  analyzer_name = local.analyzer_name
-  type          = data.aws_caller_identity.current.account_id
+# resource "aws_accessanalyzer_analyzer_org" "main_analyzer" {
+#   analyzer_name = local.analyzer_name
+#   type          = "controller"
+# }
+# resource "aws_accessanalyzer_analyzer_account" "main_analyzer" {
+#   analyzer_name = local.analyzer_name
+#   type          = data.aws_caller_identity.current.account_id
+# }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Environment = "Dev"
+  }
 }
